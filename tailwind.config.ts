131fbc38c003ac242390b7b9efa8b3e4338e8453
ken-sakura <65748538+ken-sakura@users.noreply.github.com>
2025-06-16 23:09:36 +0900

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,59 +1,20 @@
-import type { Config } from "tailwindcss";
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  darkMode: 'class', // ダークモードを有効にする
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/articles/**/*.{md}", 
+    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    // 警告を解消するためにパスの書き方を修正
+    './src/articles/**/*.md',
   ],
   theme: {
     extend: {
-      // typographyプラグインのスタイルを上書き
-      typography: ({ theme }: { theme: (path: string) => string }) => ({
-        DEFAULT: {
-          css: {
-            // ここにデフォルト（ライトモード）のスタイルを追加できます
-          },
-        },
-        invert: {
-          css: {
-            '--tw-prose-body': theme('colors.gray[300]'),
-            '--tw-prose-headings': theme('colors.white'),
-            '--tw-prose-lead': theme('colors.gray[400]'),
-            '--tw-prose-links': theme('colors.blue[400]'),
-            '--tw-prose-bold': theme('colors.white'),
-            '--tw-prose-counters': theme('colors.gray[400]'),
-            '--tw-prose-bullets': theme('colors.gray[600]'),
-            '--tw-prose-hr': theme('colors.gray[700]'),
-            '--tw-prose-quotes': theme('colors.gray[200]'),
-            '--tw-prose-quote-borders': theme('colors.gray[700]'),
-            '--tw-prose-captions': theme('colors.gray[400]'),
-            '--tw-prose-code': theme('colors.pink[300]'), // コードの色
-            '--tw-prose-pre-code': theme('colors.gray[300]'),
-            '--tw-prose-pre-bg': theme('colors.gray[900]'), // コードブロックの背景色
-            '--tw-prose-th-borders': theme('colors.gray[600]'),
-            '--tw-prose-td-borders': theme('colors.gray[700]'),
-            '--tw-prose-invert-body': theme('colors.gray[300]'),
-            '--tw-prose-invert-headings': theme('colors.white'),
-            '--tw-prose-invert-lead': theme('colors.gray[400]'),
-            '--tw-prose-invert-links': theme('colors.blue[400]'),
-            '--tw-prose-invert-bold': theme('colors.white'),
-            '--tw-prose-invert-counters': theme('colors.gray[400]'),
-            '--tw-prose-invert-bullets': theme('colors.gray[600]'),
-            '--tw-prose-invert-hr': theme('colors.gray[700]'),
-            '--tw-prose-invert-quotes': theme('colors.gray[200]'),
-            '--tw-prose-invert-quote-borders': theme('colors.gray[700]'),
-            '--tw-prose-invert-captions': theme('colors.gray[400]'),
-            '--tw-prose-invert-code': theme('colors.pink[300]'),
-            '--tw-prose-invert-pre-code': theme('colors.gray[300]'),
-            '--tw-prose-invert-pre-bg': 'rgb(0 0 0 / 50%)',
-            '--tw-prose-invert-th-borders': theme('colors.gray[600]'),
-            '--tw-prose-invert-td-borders': theme('colors.gray[700]'),
-          },
-        },
-      }),
+      backgroundImage: {
+        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+        'gradient-conic':
+          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      },
     },
   },
   plugins: [
@@ -61,4 +22,3 @@ const config: Config = {
   ],
 };
 export default config;
-
