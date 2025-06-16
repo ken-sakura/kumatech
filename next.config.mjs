fb7809867597511954a878dbaa382c29dc5302ec
ken-sakura <65748538+ken-sakura@users.noreply.github.com>
2025-06-16 22:45:19 +0900

    Update and rename next.config.ts to next.config.mjs

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  /* config options here */
+  // GitHub Pagesにデプロイするために、静的サイトとして出力する設定を追加
+  output: 'export', 
+};
+
+export default nextConfig;
